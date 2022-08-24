1f7bad0bd76c2a1d09aae62d6848049f2f64cb0e
danispinxo <genericpronoun@gmail.com>
2022-08-24 15:46:24 +0000

    sanitizing command line input

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as name, cohorts.name as cohort
-  FROM assistance_requests
-  JOIN teachers ON assistance_requests.teacher_id = teachers.id
-  JOIN students ON assistance_requests.student_id = students.id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY name;
-`)
+const queryString = `
+  SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+    FROM assistance_requests
+    JOIN teachers ON assistance_requests.teacher_id = teachers.id
+    JOIN students ON assistance_requests.student_id = students.id
+    JOIN cohorts ON students.cohort_id = cohorts.id
+    WHERE cohorts.name = $1
+    ORDER BY name
+    LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 20;
+const values = [`${cohortName}`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
